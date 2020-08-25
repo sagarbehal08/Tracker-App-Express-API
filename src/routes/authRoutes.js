96d1e83e069b8fa7ad82eb35faedda8d6acc8c51
sagarbehal08 <vivakbehal@gmail.com>
2020-08-25 21:56:01 +0530

    Error handling

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -5,10 +5,15 @@ const User =mongoose.model('User');;
 
 router.post('/signup',async(req,res)=>{
     const {email,password}=req.body;
-    const user=new User({email,password});
-    await user.save();
-    console.log(req.body);
-    res.send("You made a post request");
+    try{
+        const user=new User({email,password});
+        await user.save();
+        res.send("You made a post request");
+    } catch(err){
+        res.status(422).send(err.message);
+    }
+    
+   
 })
 
 module.exports=router;
