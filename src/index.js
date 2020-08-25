bde47e52934fba305a4c161329e0b82cc804fba3
sagarbehal08 <vivakbehal@gmail.com>
2020-08-25 12:26:41 +0530

    Intialise Mongoose and changes to start server in package.json

--- src/index.js
+++ src/index.js
@@ -1,11 +1,27 @@
 require('dotenv').config();
 const express=require('express');
+const mongoose=require('mongoose');
+
+const mongoUri='mongodb+srv://admin:passwordpassword@cluster0.xz2f1.mongodb.net/test?retryWrites=true&w=majority';
+mongoose.connect(mongoUri,{
+    useNewUrlParser:true,
+    useCreateIndex:true
+});
+
+mongoose.connection.on('connected',()=>{
+    console.log("Connected to mongo instance");
+})
+
+mongoose.connection.on('error',(err)=>{
+    console.log("Error connectiong to mongo",err);
+})
+
 const app=express();
 
 app.get("/",(req,res)=>{
     res.send("<h1>Hello World");
 })
 
-app.listen(3000,function(){
-console.log('Server start at port 3000');
+app.listen(4000,function(){
+console.log('Server start at port 4000');
 })
