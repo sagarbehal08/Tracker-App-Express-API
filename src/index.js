d1661a12b68ad51a32612bb8348a2294d93369d8
sagarbehal08 <vivakbehal@gmail.com>
2020-08-25 12:47:22 +0530

    Use body-parser to get data in json format

--- src/index.js
+++ src/index.js
@@ -1,9 +1,11 @@
 require('dotenv').config();
 const express=require('express');
 const mongoose=require('mongoose');
+const bodyParser=require('body-parser');
 const authRoutes=require('./routes/authRoutes');
 
 const app=express();
+app.use(bodyParser.json());
 app.use(authRoutes);
 
 const mongoUri='mongodb+srv://admin:passwordpassword@cluster0.xz2f1.mongodb.net/test?retryWrites=true&w=majority';
--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,8 +1,9 @@
 const express=require('express');
 const router=express.Router();
 
-router.post('signup',(req,res)=>{
-    res.send("You made a pos request");
+router.post('/signup',(req,res)=>{
+    console.log(req.body);
+    res.send("You made a post request");
 })
 
 module.exports=router;
