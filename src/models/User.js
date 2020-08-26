9d57621eb7fa17ea38c3f256651064f33d6f29d0
sagarbehal08 <vivakbehal@gmail.com>
2020-08-26 13:48:50 +0530

    Salting and Hashing using bcrypt

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-
+const bcryt=require('bcrypt');
 const userSchema=new mongoose.Schema({
     email:{
         type:String,
@@ -12,5 +12,42 @@ const userSchema=new mongoose.Schema({
     }
 })
 
+userSchema.pre('save',function(){
+    const user=this;
+    if(!user.isModified('password')){
+        return next();
+    }
+    bcrypt.genSalt(10,(err,salt)=>{
+        if(err){
+            return next(err);
+        }
+
+        bcrypt.hash(user.password,salt,(err,hash)=>{
+            if(err){
+                return next(err);
+            }
+            user.password=hash;
+            next();
+        })
+    })
+});
+
+userSchema.methods.comparePassword=function(candidatePassword){
+    const user=this;
+    return new Promise((resolve,reject)=>{
+        bcrypt.compare(candidatePassword,user.password,(err,isMatch)=>{
+            if(err){
+                return reject(err);
+            }
+
+            if(!isMatch){
+                return reject(false);
+            }
+
+            resolve(true);
+
+        });
+    });
+}
 var User = mongoose.model('User', userSchema)
 module.exports =  { User } 
