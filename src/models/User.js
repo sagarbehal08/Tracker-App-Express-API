36e565976aca3340628d6033ba979577ea7a89c9
sagarbehal08 <vivakbehal@gmail.com>
2020-08-26 14:07:14 +0530

    Solve error in salting and hashing

--- src/models/User.js
+++ src/models/User.js
@@ -1,5 +1,5 @@
 const mongoose=require('mongoose');
-const bcryt=require('bcrypt');
+const bcrypt=require('bcrypt');
 const userSchema=new mongoose.Schema({
     email:{
         type:String,
@@ -12,7 +12,7 @@ const userSchema=new mongoose.Schema({
     }
 })
 
-userSchema.pre('save',function(){
+userSchema.pre('save',function(next){
     const user=this;
     if(!user.isModified('password')){
         return next();
