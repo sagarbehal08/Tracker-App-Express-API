cff7ef296d72a4027be7ab5e1780f068811f557c
sagarbehal08 <vivakbehal@gmail.com>
2020-08-27 09:29:37 +0530

    Listing Track In routes

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 require('dotenv').config();
 require('./models/User');
+require('./models/Track');
 const express=require('express');
 const mongoose=require('mongoose');
 const bodyParser=require('body-parser');
